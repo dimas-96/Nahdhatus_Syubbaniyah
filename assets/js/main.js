feedaa15641a19b30a678fec861b47bd992635d7
dimas-96 <64664963+dimas-96@users.noreply.github.com>
2022-01-11 14:37:47 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,25 +91,25 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
+    let blogContainer = select('.blog-container');
+    if (blogContainer) {
+      let blogIsotope = new Isotope(blogContainer, {
+        itemSelector: '.blog-item'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let blogFilters = select('#blog-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#blog-flters li', function(e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        blogFilters.forEach(function(el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
 
-        portfolioIsotope.arrange({
+        blogIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
-        portfolioIsotope.on('arrangeComplete', function() {
+        blogIsotope.on('arrangeComplete', function() {
           AOS.refresh()
         });
       }, true);
